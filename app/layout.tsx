3017a3207fb8b1afcc68361f67c03491d9e44920
7se7en72025 <f20241312@pilani.bits-pilani.ac.in>
2026-06-13 13:10:30 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,10 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Kata UI",
-  description: "Edge ruler frame"
+  description: "Edge ruler frame",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
